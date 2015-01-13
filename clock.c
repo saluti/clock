c126a9b7e216b6817f6f42882ba892ff7d7b94e7
chem <chem@chem.com>
2015-01-12 19:02:23 -0800

    Update clock.c
    -added more comments 
    -fixed multiple bugs

--- clock.c
+++ clock.c
@@ -1,5 +1,6 @@
-/* Standard Clock v1 coded by Saluti */
+/* Standard Clock v1 coded by Saluti on github */
 /* Released 1/7/15 */
+/*Thanks for using */
 #include <stdio.h>
 #include <time.h>
 
@@ -13,10 +14,10 @@ int main(void) {
     time_ = localtime(&rawtime);
     
     printf("The current time is:\n");
-    printf("\n")
-    printf("\n")
+    printf("\n"); /* to make the program look clean */
+    printf("\n");
     printf("%i:%i \n", time_->tm_hour, time_->tm_min);
-    printf("\n")
-    printf("\n")
+    printf("\n");
+    printf("\n");
     printf("Rest is important! Make sure to take a break once in awhile!\n");
 }
