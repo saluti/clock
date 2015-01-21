7c9fe08a5d9d052f1515b4d72e3a7b0f0f6294c2
chem <chem@chem.com>
2015-01-21 12:09:11 -0800

    Update clock.c
    added more info

--- clock.c
+++ clock.c
@@ -1,8 +1,8 @@
 /* Standard Clock v1 coded by Saluti on github */
 /* Released 1/7/15 */
 /*Thanks for using */
-#include <stdio.h>
-#include <time.h>
+#include <stdio.h> /* To include stdio.h */
+#include <time.h> /* To include time.h */
 
 
 int main(void) {
@@ -10,14 +10,14 @@ int main(void) {
     time_t rawtime;
     struct tm*  time_;
     
-    time(&rawtime);
-    time_ = localtime(&rawtime);
+    time(&rawtime); /* Uses time.h */
+    time_ = localtime(&rawtime); /*To find the user's local time! */
     
-    printf("The current time is:\n");
+    printf("The current time is:\n"); /* text to display the current time */
     printf("\n"); /* to make the program look clean */
     printf("\n");
-    printf("%i:%i \n", time_->tm_hour, time_->tm_min);
+    printf("%i:%i \n", time_->tm_hour, time_->tm_min); /*shows as hour:min no seconds */
+    printf("\n");/* to make the program look clean */
     printf("\n");
-    printf("\n");
-    printf("Rest is important! Make sure to take a break once in awhile!\n");
-}
+    printf("Rest is important! Make sure to take a break once in awhile!\n"); /*reminder!*/
+} /*end of code */
